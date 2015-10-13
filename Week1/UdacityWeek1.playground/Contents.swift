9c173d77c1a388d96bb2d2255668ae332d6de9b0
ishantrana <ishant@atcouch.com>
2015-10-14 01:32:42 +0530

    week1 start

---
+++ Week1/UdacityWeek1.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+var hungry = true
+var vegetarian = true
+
+if hungry {
+    print("lets eat")
+}else{
+    print("lets wait")
+}
+
+
+if hungry && !vegetarian{
+    print("lets eat stake")
+}else{
+    print("nevermind")
+} 
+
+hungry ? print("lets eat") : print("lets wait")
+
+let similar = "🂄"
+
+var dogName = "ferris"
+
+var dogName2 = "dog1"
+
+var dogEats = "\(dogName) eats \(382939+392392) \(dogName2)"
+
+
